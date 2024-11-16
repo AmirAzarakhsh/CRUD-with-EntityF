7f9f975cb3f544cc3af08545fc88d5f21e4072ff
AmirhosseinAzarakhsh <149069544+AmirAzarakhsh@users.noreply.github.com>
2024-11-16 11:26:20 +0330

    Update Program.cs

--- Introduction/Program.cs
+++ Introduction/Program.cs
@@ -1,11 +1,11 @@
 ﻿using Introduction.Models;
 
 Ripository ripo = new Ripository();
-//ripo.UpdatePerson(2 , "mahan" , "jafarzadeh");
-//ripo.UpdatePerson(5, "AmirhosseinUpdate", "Azarakhsh"); when null
-//ripo.AddPerson("mahan","jafarzadeh");
-//ripo.AddPerson("mehrshad" , "torkamanzade");
+ripo.UpdatePerson(2 , "mahan" , "jafarzadeh");
+
+ripo.AddPerson("mahan","jafarzadeh");
 ripo.PrintPeople();
 Console.WriteLine("Select the Id you want to delete");
 int id = Convert.ToInt32(Console.ReadLine());
 ripo.DeleteMembers(id);
+Console.ReadLine();
